42c6595c55b1be5bcad4b55dbb3e522fba1c77d6
KoorunG <onlyzion10@naver.com>
2022-11-24 17:04:23 +0900

    fix : 서버에 회원가입 요청 보내도록 수정

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -1,24 +1,68 @@
 import { Box, Button, Container, Heading, Input, Stack, Text } from '@chakra-ui/react';
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import AddressSearchPopup from '../components/AddressSearchPopup';
 
 const Join: React.FC = () => {
+	const [userId, setUserId] = useState('');
+	const [password, setPassword] = useState('');
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [phoneNumber, setPhoneNumber] = useState('');
+
+	interface IJoinRequest {
+		userId: string;
+		password: string;
+		name: string;
+		email: string;
+		phoneNumber?: string;
+		address?: IAddress;
+	}
+
+	interface IAddress {
+		sido: string;
+		signgu: string;
+		dong: string;
+	}
+
+	interface IExceptionInfo {
+		errorCode: string;
+		fieldName: string;
+		message: string;
+	}
+	const joinRequest: IJoinRequest = { userId, password, name, email, phoneNumber };
+
+	const postJoinRequest = async () => {
+		const confirm = window.confirm('회원가입을 진행하시겠습니까?');
+		if (!confirm) return;
+
+		await axios.post('http://localhost:8080/join', joinRequest).catch(error => {
+			if (error) {
+				const data: IExceptionInfo[] = error.response.data.exceptionInfoList;
+				for (const info of data) {
+					console.table(info);
+				}
+			}
+		});
+	};
+
 	return (
 		<Container maxW="container.lg" centerContent>
 			<Heading>회원가입</Heading>
+
 			<Stack mt={10} gap={2}>
-				<CustomInput title="아이디" placeholder="영문, 숫자 5-11자" required />
-				<CustomInput title="비밀번호" placeholder="숫자, 영문, 특수문자 조합 최소 8자" required />
+				<CustomInput title="아이디" placeholder="영문, 숫자 5-11자" required onChangeFn={setUserId} />
+				<CustomInput title="비밀번호" placeholder="숫자, 영문, 특수문자 조합 최소 8자" required onChangeFn={setPassword} />
 				<CustomInput placeholder="비밀번호 재입력" required />
-				<CustomInput title="이름" required />
-				<CustomInput title="전화번호" placeholder="전화번호 입력" />
-				<CustomInput title="이메일" required />
+				<CustomInput title="이름" required onChangeFn={setName} />
+				<CustomInput title="전화번호" placeholder="전화번호 입력" onChangeFn={setPhoneNumber} />
+				<CustomInput title="이메일" required onChangeFn={setEmail} />
 				<Text fontSize={12} colorScheme="gray">
 					계정분실 시 본인인증 정보로 활용됩니다.
 				</Text>
 				<CustomInput title="지역" placeholder="[주소찾기] 버튼으로 입력" disabled />
 				<AddressSearchPopup />
-				<Button>가입하기</Button>
+				<Button onClick={postJoinRequest}>가입하기</Button>
 			</Stack>
 		</Container>
 	);
@@ -30,10 +74,10 @@ interface ICustomInputProps {
 	required?: boolean;
 	value?: string;
 	disabled?: boolean;
-	onChangeFn?: () => {};
+	onChangeFn?: (input: string) => void;
 }
 
-const CustomInput = ({ title, placeholder, required, value, disabled }: ICustomInputProps): JSX.Element => {
+const CustomInput = ({ title, placeholder, required, value, disabled, onChangeFn }: ICustomInputProps): JSX.Element => {
 	return (
 		<>
 			{title ? (
@@ -46,7 +90,16 @@ const CustomInput = ({ title, placeholder, required, value, disabled }: ICustomI
 					) : null}
 				</Box>
 			) : null}
-			<Input placeholder={placeholder} onChange={e => {}} height={50} width={350} value={value} readOnly={disabled} />
+			<Input
+				placeholder={placeholder}
+				onChange={e => {
+					if (onChangeFn) onChangeFn(e.currentTarget.value);
+				}}
+				height={50}
+				width={350}
+				value={value}
+				readOnly={disabled}
+			/>
 		</>
 	);
 };
